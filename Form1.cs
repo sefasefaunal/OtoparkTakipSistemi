2f0c271ea16342fbf33283253bed9d3cc551b58d
sefasefaunal <unals5370@gmail.com>
2025-07-05 12:04:12 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+using System.Drawing.Text;
+
+namespace BST208_OtoparktTakipSistemi
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+    }
+}
